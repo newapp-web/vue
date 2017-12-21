614c57e99ff7517ec2f31b735a82fcb6da8131b9
newapp-web <wagnshiwei@yahoo.com>
2017-12-21 14:10:17 +0800

    fix

--- template/main.js
+++ template/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
-import App from '../components/App';
+import App from './components/App';
 {{#router}}
-import router from '../router';
+import router from './router';
 {{/router}}
 {{#resource}}
 import resource from 'vue-resource';
